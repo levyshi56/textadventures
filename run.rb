ca74d9ce2de20e2f71ac2bff6908bb6d58fb6fdb
levyshi56 <levys19@gmail.com>
2015-05-16 19:18:05 -0400

    Update run.rb

--- run.rb
+++ run.rb
@@ -1,1 +1,15 @@
+#welcome page
+puts "Welcome to an adventure created in one night"
+sleep (1)
+puts "This is an interacive adventure created by Levy Shi"
+sleep (1)
+#options for the user
+user_choice = gets.chomp
+#instructions fot the user
+puts "the rules of the game are easy, just type in your answer to procced to the next part of your adventure"
+#beginning of the adventure
+puts "You start an adventure in a dense forrest, you are searching for the princess of your kingdom"
+sleep (2)
+puts "You walk into the forrest and you come across a fork in the road, do you go left or right?"
 
+  
